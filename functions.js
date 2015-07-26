cb878c663ed0e898d7b6e76184d3451b4cc4e4b2
jerh56 <jerh56@gmail.com>
2015-07-25 20:06:25 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -10,12 +10,12 @@ var bMap = false;
 
 $(document).ready(function() {
 		inicializar();
-		setInterval(revisarSensores(), 3000);
+	
 });
 
 
 function inicializar() {
-  
+  	setInterval(revisarSensores(), 3000);
 };
 
 function revisarSensores() {
@@ -34,7 +34,7 @@ $.ajaxSetup({
 });
     
 $.ajax({
-         url: "http://orion.lab.fi-ware.org:1026/ngsi10/contextEntities/plant00001", type: 'GET'
+         url: "http://orion.lab.fi-ware.org:1026/ngsi10/contextEntities/plant00002", type: 'GET'
     }).then(function(data) {
         //alert(JSON.stringify(data));
         colocaHumedad(data); 
@@ -52,13 +52,31 @@ $.ajaxSetup({
 });
     
 $.ajax({
-         url: "http://orion.lab.fi-ware.org:1026/ngsi10/contextEntities/plant00001", type: 'GET'
+         url: "http://orion.lab.fi-ware.org:1026/ngsi10/contextEntities/plant00002", type: 'GET'
     }).then(function(data) {
         //alert(JSON.stringify(data));
         colocaTemperatura(data); 
     });
 };      
 
+
+function revisarTierra() {      
+
+$.ajaxSetup({
+  beforeSend: function(request) {
+    request.setRequestHeader("X-Auth-Token","VMfS1db5lyV3mtJ7YSqAqPFHR9cu9m");
+    request.setRequestHeader("Accept","application/json");
+    request.setRequestHeader("Content-Type","application/json"); 
+  }
+});
+    
+$.ajax({
+         url: "http://orion.lab.fi-ware.org:1026/ngsi10/contextEntities/plant00002", type: 'GET'
+    }).then(function(data) {
+        //alert(JSON.stringify(data));
+        colocaTemperatura(data); 
+    });
+};     
 
 function colocaHumedad(data){
      var humedad =  data.contextElement.attributes[0].value;
@@ -70,4 +88,10 @@ function colocaTemperatura(data){
     var temperatura =  data.contextElement.attributes[1].value;
     $('#tvTemperatura').html("Temperatura: " + temperatura);
     $('#tvResponse').html(JSON.stringify(data));
+};
+
+function colocaTierra(data){
+     var tierra =  data.contextElement.attributes[2].value;
+     $('#tvTierra').html("Humedad tierra: " + tierra);
+     $('#tvResponse').html(JSON.stringify(data));
 };
